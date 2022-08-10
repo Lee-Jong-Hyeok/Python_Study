9890edecb4aa1811b2d878642debc62c5f46e6e3
lee-jong-hyeok <m3675@naver.com>
2022-08-10 10:32:55 +0900

    클래스 연습3

--- isinstace함수_파생여부확인.py
+++ isinstace함수_파생여부확인.py
@@ -1,8 +1,8 @@
-class Person:
+class Person: # class Person(object) 동일, object 클래스가 기본적으로 들어가기 때문에 생략
     pass
 class Bird:
     pass
-class Student(Person):
+class Student(Person): #Person을 Student가 상속
     pass
 p, s = Person(), Student()
 
