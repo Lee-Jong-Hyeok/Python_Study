3c2646e4733053ba14d59417c228282efe1eb0bd
lee-jong-hyeok <m3675@naver.com>
2022-08-11 10:04:32 +0900

    문자열 전처리 메서드 연습

---
+++ DemoStr.py
@@ -0,0 +1,31 @@
+# DemoStr.py
+import re
+
+
+strA = "python is very powerful"
+strB = "파이썬은 강력해"
+print( len(strA) )
+print( len(strB) )
+print( strA.capitalize() ) # 첫문자를 대문자로 변경
+print( strA.count("p") ) # p가 몇번 나왔는지
+print( strA.count("p", 7) ) # ?
+print( strA.startswith("py") ) #py로 시작?
+print( strA.endswith("ful") ) #py로 끝나?
+
+print( "MBC2580".isnumeric() )
+print( "MBC:2580".isnumeric() )
+print( "2580".isnumeric() )
+
+#문자열 전처리(데이터)
+strC = "<<< spam and ham >>>"
+result = strC.strip("<> ")
+print(strC)
+print(result)
+
+#문자열 전처리(데이터)
+print("---치환---")
+result = result.replace("spam","spam egg")
+print(result)
+lst = result.split()
+print("---다시 합치기---")
+print( ":)".join(lst) )
