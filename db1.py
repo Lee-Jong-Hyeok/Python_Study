e8be7105c2492c5eb41f8de467969d261d18d4d1
lee-jong-hyeok <m3675@naver.com>
2022-08-11 14:19:44 +0900

    데이터베이스 연습

---
+++ db1.py
@@ -0,0 +1,17 @@
+# db1.py
+import sqlite3
+
+#연결객체 생성
+con = sqlite3.connect(":memory:")
+
+#커서객체 생성
+cur = con.cursor()
+
+#테이블 구조(테이블 스키마 생성)
+cur.execute("create table phoneBook (Name text, phoneNum text);")
+#1건 레코드(행)을 입력
+cur.execute("insert into PhoneBook values ('derick', '010-111');")
+#검색
+cur.execute("select * from phoneBook;")
+for row in cur:
+    print(row)
