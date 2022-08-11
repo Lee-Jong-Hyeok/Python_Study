0d2e5950260c5ea093f84d3556afae20e8d35515
lee-jong-hyeok <m3675@naver.com>
2022-08-11 14:23:48 +0900

    데이터베이스 연습3

--- db1.py
+++ db1.py
@@ -9,6 +9,11 @@ cur = con.cursor()
 
 #테이블 구조(테이블 스키마 생성)
 cur.execute("create table phoneBook (Name text, phoneNum text);")
+
+#입력 파라메터 처리 (웹페이지, 모바일앱)
+name = "gildong"
+phoneNumber = "001-222"
+cur.execute( "insert into phoneBook values (?,?);", (name, phoneNumber) )
 #1건 레코드(행)을 입력
 cur.execute("insert into PhoneBook values ('derick', '010-111');")
 #검색
