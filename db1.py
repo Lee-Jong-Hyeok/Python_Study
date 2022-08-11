ad475f09dbbb70520e365ac5b8df2a65e581826c
lee-jong-hyeok <m3675@naver.com>
2022-08-11 14:52:07 +0900

    데이터베이스 완료

--- db1.py
+++ db1.py
@@ -1,8 +1,9 @@
 # db1.py
 import sqlite3
-
-#연결객체 생성
-con = sqlite3.connect(":memory:")
+#연결객체 생성(메모리에 저장)
+# con = sqlite3.connect(":memory:")
+#연결객체 생성(물리적인 파일에 저장)
+con = sqlite3.connect("c:\\work\\commit.db")
 
 #커서객체 생성
 cur = con.cursor()
@@ -20,3 +21,6 @@ cur.execute("insert into PhoneBook values ('derick', '010-111');")
 cur.execute("select * from phoneBook;")
 for row in cur:
     print(row)
+
+#작업을 정상적으로 완료(입력, 수정, 삭제)
+con.commit()
