ddf2ff74743550f6284b5792deafcdb0ee938275
lee-jong-hyeok <m3675@naver.com>
2022-08-12 09:42:37 +0900

    웹크롤링 연습2

--- web1.py
+++ web1.py
@@ -1,17 +1,26 @@
 # web1.py
 # 웹크롤링(데이터 수집)
-from bs4 import BeautifulStoneSoup
+from bs4 import BeautifulSoup
 
 #메서드 체인:연속으로 함수나 메서드 호출
 page = open("c:\\work\\test01.html", "rt", encoding="utf-8").read()
 #검색이 용이한 객체
-soup = BeautifulStoneSoup(page, "html.parse") # "html.parse" -> 정해진 규칙
+soup = BeautifulSoup(page, "html.parse") 
+#"html.parse" -> 정해진 규칙
 # 문서 정렬
 # print( soup.prettify() )
 # <P>를 몽땅 검색  
 # print( soup.find_all("P") )  
 # 첫번째 <P>태그 
+# print( soup.find("P") )# 문서 정렬
+# print( soup.prettify() )
+# <P>를 몽땅 검색  
+# print( soup.find_all("P") )  
+# 첫번째 <P>태그 
 # print( soup.find("P") )
+# 조건이 있는: <P class='outer-text'> 필터링
+# 파이썬의 키워드 class --> class_
+# print( soup.find_all("P", class_="outer-text") )
 # 조건이 있는: <P class='outer-text'> 필터링
 # 파이썬의 키워드 class --> class_
 # print( soup.find_all("P", class_="outer-text") )
